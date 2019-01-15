00d3d9d5ca9669d20ccb3cdf11753391e65c5548
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-01-16 00:19:04 +0300

    scroll onclick hello btn

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 $('.menu-btn').on('click', function(e) {
-  // e.preventDefault;
-  $(this).toggleClass('menu-btn_active');
-  $(".menu").toggleClass("menu_active");
+	$(this).toggleClass('menu-btn_active');
+	$(".menu").toggleClass("menu_active");
 });
+
 $('.hello_btn').on('click', function(e) {
-  $(".hello").css({'transform':'scale(20)'});
+	$('.all, header').show();
+	// $('html, body').css({'overflow-y':'auto'});
+	$('.hello').hide(1000);
+	
+
+  	$('html, body').animate({scrollTop: $('#kek').position().top+'px'}, 800)
+ 		
 });
+
 
 
