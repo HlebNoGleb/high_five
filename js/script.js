e9608ba58051351f2ef62b5f0db443e8c62e45b3
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-01-15 23:15:04 +0300

    restart

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ $('.menu-btn').on('click', function(e) {
   // e.preventDefault;
   $(this).toggleClass('menu-btn_active');
   $(".menu").toggleClass("menu_active");
-  $(".cols").toggleClass( "open_it");
-  $(".call_block").toggleClass("call_block_active")
+});
+$('.hello_btn').on('click', function(e) {
+  $(".hello").css({'transform':'scale(20)'});
 });
 
 
