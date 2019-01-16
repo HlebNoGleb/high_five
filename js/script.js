ba269078cdd139d54690e59132cdc5fb87ab4179
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-01-16 11:32:45 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $('.menu-btn').on('click', function(e) {
 	$(this).toggleClass('menu-btn_active');
-	$(".menu").toggleClass("menu_active");
+	$(".menu").addClass("menu_active");
 });
 
  $('.hello_btn').on('click', function(e) {
