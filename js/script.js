04fba528f39a4faacc9ea53aa0716bf42f280245
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-01-16 12:58:11 +0300

    loading

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $('.menu-btn').on('click', function(e) {
 	$(this).toggleClass('menu-btn_active');
-	$(".menu").toggleClass("menu_active",400);
+	$(".menu").toggleClass("menu_active");
 });
 
  $('.hello_btn').on('click', function(e) {
@@ -8,10 +8,14 @@ $('.menu-btn').on('click', function(e) {
 // 	// $('html, body').css({'overflow-y':'auto'});
 	$('.hello').fadeOut(1000);
 	setTimeout(function(){
+		 		$(".load").fadeIn(200);
+		 	}, 1000);
+	setTimeout(function(){
 		 		$('.all').fadeIn(1000); 
 		 		$('header').slideDown(1000); 
 		 		$('html, body').css({'overflow-y':'auto'});
-		 	}, 1000);
+		 		$(".load").fadeOut(200);
+		 	}, 3000);
 	// $('.all').scrollDow(1000); 		
  });
 // $(".hello").on("click","a", function (event) {
