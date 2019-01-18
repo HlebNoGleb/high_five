adfec572300dd64b2335494c674bbf93d67fb32e
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-01-18 11:39:58 +0300

    add preloader

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+document.body.onload = function(){
+	setTimeout(function(){
+		var preloader = document.getElementById('page-preloader');
+		if(!preloader.classList.contains('done')){
+			preloader.classList.add('done');
+		}
+		AOS.init();
+	}, 2000);
+}
 $('.menu-btn').on('click', function(e) {
 	$(this).toggleClass('menu-btn_active');
 	$(".menu").toggleClass("menu_active");
 });
-
  $('.hello_btn').on('click', function(e) {
-// 	$('.all, header').show();
-// 	// $('html, body').css({'overflow-y':'auto'});
 	$('.hello').fadeOut(1000);
 	setTimeout(function(){
 		 		$(".load").fadeIn(200);
@@ -15,17 +21,5 @@ $('.menu-btn').on('click', function(e) {
 		 		$('header').slideDown(1000); 
 		 		$('html, body').css({'overflow-y':'auto'});
 		 		$(".load").fadeOut(200);
-		 	}, 3000);
-	// $('.all').scrollDow(1000); 		
+		 	}, 3000);		
  });
-// $(".hello").on("click","a", function (event) {
-		// $('html').css({'overflow-y':'scroll'});
-		// event.preventDefault();        
-  //       var id  = $(this).attr('href');
-  //       var  top = $(id).offset().top;
-  //       $('body,html').animate({scrollTop: top}, 1500);
-		// setTimeout(function(){
-		// 		$('.hello').hide(0);
-		// 	}, 1000);	
-
-    // });
