c866eed5f8ed9e01b88efc000acd28c36d9d254f
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-01-30 12:26:43 +0300

    horizontalscroll

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-document.body.onload = function(){
-	setTimeout(function(){
-		var preloader = document.getElementById('page-preloader');
-		if(!preloader.classList.contains('done')){
-			preloader.classList.add('done');
-		}
-		AOS.init();
-	}, 2000);
-}
+// document.body.onload = function(){
+// 	setTimeout(function(){
+// 		var preloader = document.getElementById('page-preloader');
+// 		if(!preloader.classList.contains('done')){
+// 			preloader.classList.add('done');
+// 		}
+// 		AOS.init();
+// 	}, 2000);
+// }
 $('.menu-btn').on('click', function(e) {
 	$(this).toggleClass('menu-btn_active');
 	$(".menu").toggleClass("menu_active");
@@ -23,3 +23,33 @@ $('.menu-btn').on('click', function(e) {
 		 		$(".load").fadeOut(200);
 		 	}, 3000);		
  });
+ $('.experimental').on('click', function(e) {
+ 	$(this).removeClass('experimental');
+ });
+ function colsWidth() {
+
+
+
+
+
+ 	var colCount;
+ 	var colsW;
+  	colCount = $('.cols .col').length;
+  	colsW = colCount * 33;
+   $('.cols').css('width', ''+colsW+'vw');
+
+
+
+
+   $('.cols').css({'overflow-x':'scroll'});
+   $('.cols').css({'flex-wrap':'nowrap'});
+   $('.cols').css({'flex-direction':'row'});
+   $('body').css({'overflow-x':'scroll'});
+   $('html').css({'overflow':'hidden'});
+   $('html').css({'scroll-direction':'horizontal'});
+
+
+
+
+}
+
