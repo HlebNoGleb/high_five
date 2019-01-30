b81ada34299a75eb7c6b5623d985ed0be3f18671
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-01-30 15:32:28 +0300

    add beta features in prod

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-// document.body.onload = function(){
-// 	setTimeout(function(){
-// 		var preloader = document.getElementById('page-preloader');
-// 		if(!preloader.classList.contains('done')){
-// 			preloader.classList.add('done');
-// 		}
-// 		AOS.init();
-// 	}, 2000);
-// }
+document.body.onload = function(){
+	setTimeout(function(){
+		var preloader = document.getElementById('page-preloader');
+		if(!preloader.classList.contains('done')){
+			preloader.classList.add('done');
+		}
+		AOS.init();
+	}, 2000);
+}
 $('.menu-btn').on('click', function(e) {
 	$(this).toggleClass('menu-btn_active');
 	$(".menu").toggleClass("menu_active");
@@ -19,6 +19,7 @@ $('.menu-btn').on('click', function(e) {
 	setTimeout(function(){
 		 		$('.all').fadeIn(1000); 
 		 		$('header').slideDown(1000); 
+		 		$('.beta').fadeIn(1000);
 		 		$('html, body').css({'overflow-y':'auto'});
 		 		$(".load").fadeOut(200);
 		 	}, 3000);		
@@ -52,8 +53,9 @@ $('.menu-btn').on('click', function(e) {
   };
 });
 
-$(document).ready(function() {
-  $('body').hScroll(60); // You can pass (optionally) scrolling amount
+$(document).ready(function(colsW) {
+	console.log(colsW);
+  $('body').hScroll(100); // You can pass (optionally) scrolling amount
 });
 }
 
